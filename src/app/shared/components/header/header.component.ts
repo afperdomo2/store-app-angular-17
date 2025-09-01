dc5111a558e17a0a6bcd27f18e19f84b06053da0
alvarado0925 <alvarado0925@gmail.com>
2025-09-01 17:44:49 -0500

    feat: Implementa sidebar de carrito con funcionalidad de apertura y cierre

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  isCartSidebarOpen = signal(false);
 
+  toggleCartSidebar() {
+    this.isCartSidebarOpen.update((value) => !value);
+  }
+
+  closeCartSidebar() {
+    this.isCartSidebarOpen.set(false);
+  }
 }

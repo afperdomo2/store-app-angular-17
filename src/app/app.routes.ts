80fdc11e8929265c120c43cbc29b4059cf1d11dd
alvarado0925 <alvarado0925@gmail.com>
2025-09-11 15:49:06 -0500

    feat: Implementa LayoutComponent y reorganiza rutas para mejorar la estructura de la aplicación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,16 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from '@features/info/pages/about/about.component';
 import { NotFoundComponent } from '@features/info/pages/not-found/not-found.component';
 import { ListComponent } from '@features/products/pages/list/list.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', component: ListComponent },
-  { path: 'about', component: AboutComponent },
-  { path: '**', component: NotFoundComponent }, // Wildcard route for 404 page
+  {
+    path: '',
+    component: LayoutComponent, // Layout reutilizable con Header
+    children: [
+      { path: '', component: ListComponent },
+      { path: 'about', component: AboutComponent },
+    ],
+  },
+  { path: '**', component: NotFoundComponent }, // Ruta para manejar páginas no encontradas
 ];

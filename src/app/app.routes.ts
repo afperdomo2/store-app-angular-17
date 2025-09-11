f11ac9e0229c0a999c391f986dba0d36471dab48
alvarado0925 <alvarado0925@gmail.com>
2025-09-11 15:31:32 -0500

    feat: Añade componente NotFound con su plantilla y estilos para manejar errores 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './features/info/pages/about/about.component';
-import { ListComponent } from './features/products/pages/list/list.component';
+
+import { AboutComponent } from '@features/info/pages/about/about.component';
+import { NotFoundComponent } from '@features/info/pages/not-found/not-found.component';
+import { ListComponent } from '@features/products/pages/list/list.component';
 
 export const routes: Routes = [
   { path: '', component: ListComponent },
   { path: 'about', component: AboutComponent },
+  { path: '**', component: NotFoundComponent }, // Wildcard route for 404 page
 ];
